405cf997196feb33790c7880abdeb736009624c4
Smruthi Rao <smruthisumanthrao@gmail.com>
2024-06-03 14:41:35 +0530

    Halleluja!

--- generate.py
+++ generate.py
@@ -1,41 +1,25 @@
 import subprocess
-from wolframclient.language import wl, wlexpr
 
-def create_wolfram_command(equation, file_name):
-    """
-    Formats the Wolfram Language equation into a command for exporting a 3D plot as an OBJ file.
+def export_3d_plot(equation, x_min, x_max, y_min, y_max, filename):
+    # Define the Wolfram command with the given parameters
+    wolfram_command = f'Export["{filename}.obj", Plot3D[{equation}, {{x, {x_min}, {x_max}}}, {{y, {y_min}, {y_max}}}, PlotRange -> All], "Graphics3DObject"]'
 
-    Args:
-        equation (str): The Wolfram Language mathematical equation.
-        file_name (str): The desired file name for the exported OBJ file.
-
-    Returns:
-        str: The formatted Wolfram command.
-    """
-    # Format the Wolfram command
-    wolfram_command = f'Export["C:\\Users\\User\\Desktop\\{file_name}.obj", Plot3D[{equation}, {{x, -Pi, Pi}}, {{y, -Pi, Pi}}], "Graphics3DObject"]'
-    return wolfram_command
-
-def export_3d_model(equation, file_name):
-    """
-    Executes the Wolfram command to export the 3D plot of the given equation as an OBJ file.
-
-    Args:
-        equation (str): The Wolfram Language mathematical equation to be plotted in 3D.
-        file_name (str): The desired file name for the exported OBJ file.
-    """
-    # Create the Wolfram command
-    wolfram_command = create_wolfram_command(equation, file_name)
-    
     # Run the Wolfram command using subprocess
-    try:
-        subprocess.run(['wolframscript', '-code', wolfram_command], check=True)
-        print(f"3D model exported to {file_name}.obj")
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred while executing the Wolfram command: {e}")
+    result = subprocess.run(['wolframscript', '-code', wolfram_command], capture_output=True, text=True)
+    
+    if result.returncode == 0:
+        print(f"3D model exported to: {filename}.obj")
+    else:
+        print("Error in exporting 3D model:")
+        print(result.stderr)
 
+# Example usage
+if __name__ == "__main__":
+    equation = input("Enter the equation (in terms of x and y): ")
+    x_min = float(input("Enter the minimum value for x: "))
+    x_max = float(input("Enter the maximum value for x: "))
+    y_min = float(input("Enter the minimum value for y: "))
+    y_max = float(input("Enter the maximum value for y: "))
+    filename = input("Enter the output filename (without .obj extension): ")
 
-# User input for the natural language equation and file name
-user_query = input("Enter the mathematical equation in wolfram: ")
-file_name = input("Enter the desired file name for the OBJ file: ")
-export_3d_model(user_query, file_name)
+    export_3d_plot(equation, x_min, x_max, y_min, y_max, filename)
